6603c81fafbbde16006e0c2ddcaeb141d18af519
Waqar Ahamd <w.waqarahmad786@gmail.com>
2020-09-11 07:04:28 -0700

    Contact Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Home from "./Home"
 import Service from "./Service"
+import Contact from "./Contact"
 
 function App() {
   return (
     <div>
       <Home />
       <Service />
+      <Contact />
     </div>
   );
 }
