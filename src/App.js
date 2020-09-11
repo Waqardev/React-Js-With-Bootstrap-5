1373926b36513a7b0cb3a4ff2c5b9053691676a0
Waqar Ahamd <w.waqarahmad786@gmail.com>
2020-09-11 06:59:41 -0700

    Service Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Home from "./Home"
+import Service from "./Service"
 
 function App() {
   return (
     <div>
       <Home />
+      <Service />
     </div>
   );
 }
---
+++ src/Service.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Service = () => {
+  return (
+    <div>
+      <h1>I'm Service Page</h1>
+    </div>
+  );
+};
+export default Service;
