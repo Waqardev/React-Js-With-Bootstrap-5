b9fe8c13f95c4bd7d83388d7490938122fadc6dd
Waqar Ahamd <w.waqarahmad786@gmail.com>
2020-09-11 07:25:44 -0700

    Routing Added

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import Home from "./Home"
 import Service from "./Service"
 import Contact from "./Contact"
 import About from "./About"
+import { Switch, Route, Redirect } from "react-router-dom"
 
 function App() {
   return (
     <div>
-      <Home />
-      <Service />
-      <Contact />
-      <About />
+      <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/service" component={Service} />
+      <Route exact path="/contact" component={Contact} />
+      <Route exact path="/about" component={About} />
+      <Redirect to="/" />
+      </Switch>
     </div>
   );
 }
