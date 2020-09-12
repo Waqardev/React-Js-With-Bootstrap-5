c0f11187e743c3749edfb91b831c323462443da0
Waqar Ahamd <w.waqarahmad786@gmail.com>
2020-09-12 03:27:24 -0700

    Navbar Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 import Home from "./Home"
 import Service from "./Service"
 import Contact from "./Contact"
 import About from "./About"
+import Navbar from "./Navbar";
 import { Switch, Route, Redirect } from "react-router-dom"
 
 function App() {
   return (
     <div>
+      <Navbar />
       <Switch>
       <Route exact path="/" component={Home} />
       <Route exact path="/service" component={Service} />
